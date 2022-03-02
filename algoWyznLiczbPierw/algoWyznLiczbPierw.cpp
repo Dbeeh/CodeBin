69defc939bc04bda3bf7421aebea427e18ac0396
Zuzanna Czerniawska (Uczeń) <dark.blizzard.pl@gmail.com>
2022-03-02 15:06:48 +0100

    aaaaaaaaa

---
+++ algoWyznLiczbPierw/algoWyznLiczbPierw.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+    using namespace std;
+
+    
+int main()
+{
+    int lp, p, d, n;
+        cout << "ile liczb pierwszych wygenerowac: ";
+                cin >> n;       
+
+
+        
+    
+}
