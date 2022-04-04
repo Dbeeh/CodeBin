e06c840d5711a36fb715fa9229ec24005fcce145
Kamil Mnich (Uczeń) <kmnich457@technikumkreatywne.pl>
2022-04-04 10:57:23 +0200

    BRUHHH

---
+++ Algortym wyznaczania liczb pierwszych/Algortym wyznaczania liczb pierwszych/Algortym wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,37 @@
+﻿// Algortym wyznaczania liczb pierwszych.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int lp = 0;
+    int p = 2; 
+    int d = 1;
+
+    cout << "Ile liczb pierwszych chcesz wyswietlic : ";
+    cin >> n;
+    cout << "Liczby pierwsze: ";
+
+    while (lp < n)
+    {
+        for (int i = 2; i < p - 1; i++)
+        {
+
+
+            d = d + 1;
+            if (p % d == 0)
+            {
+                p = p + 1;
+                d = 1;
+            }
+        }
+        cout << p << " ";
+        p = p + 1;
+        lp = lp + 1;
+        d = 1;
+    }
+
+}
---
+++ NWW i NWD/NWW i NWD.cpp
@@ -0,0 +1,50 @@
+﻿// NWW i NWD.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int iloczyn;
+
+	cout << "NWD" << endl;
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> a;
+	cout << "Podaj druga liczbe : ";
+	cin >> b;
+
+	while (a != b)
+	{
+		if (a < b)
+		{
+			b = b - a;
+		}
+		else
+		{
+			a = a - b;
+		}
+	}
+	cout << "Najwiekszy wspolny dzielnik: " << a;
+
+	iloczyn = a * b;
+	while (a != b)
+	{
+		if (a < b)
+		{
+			b = b - a
+		}
+
+		else
+		{
+			a = a - b
+		}
+		
+		nww = iloczyn / a
+	}
+	cout << "Najwieksza wspolna wielokrotnosc" << a;
+	
+}
+
